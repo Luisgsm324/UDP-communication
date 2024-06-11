f675f71da45eb1734f4943aaf54f48d62ed39cc3
Vitor Mendonça <83474640+VitorMendonca62@users.noreply.github.com>
2024-06-11 17:38:03 -0300

    Realizando a moda Mateus Guerra - Corrigindo código de Luis

--- client.py
+++ client.py
@@ -2,30 +2,23 @@ from socket import *
 import threading
 import time
 
-SERVERNAME = 'localhost'
+SERVERNAME = '172.20.4.17'
 SERVERPORT = 5000
 BUFFER_SIZE = 1024 # Tamanho do buffer especificado pelos requisitos do projeto
 # Socket do client
 # AF_INET -> USANDO IPV4
 # SOCK_DGRAM -> PROTOCOLO UDP
 client = socket(AF_INET, SOCK_DGRAM)
-queue_content = []
-
-def check_content():
-    if len(queue_content) != 0:
-        print(queue_content[0])
-        queue_content.pop(0)
-
 # Receber conteúdo
 # Precisa rodar o tempo todo (pode receber mensagem a qualquer momento)
+
 def receive_content():
     try:
         while True:
             output, serveradress = client.recvfrom(BUFFER_SIZE)
             time.sleep(0.5)
             content = output.decode()
-            queue_content.append(content)
-            check_content()
+            print(content)
     except:
         pass
 
@@ -35,6 +28,7 @@ def handle_input_content():
     while True:
         data = input("")
         if 'bye' == data: 
+            send_content(data)
             client.close()
             break
         data = f"{name}: {data}"
@@ -52,10 +46,15 @@ def enter_session(name):
     content = f"O usuário {name} entrou na sala"
     send_content(content)   
 
-name = input("Insira o seu nome: ")
-enter_session(name)
+name = ""
+def main():
+    name = input("Insira o seu nome: ")
 
-thread_receive = threading.Thread(target=receive_content)
-thread_receive.start()
-thread_send = threading.Thread(target=handle_input_content)
-thread_send.start()
+    enter_session(name)
+
+    thread_receive = threading.Thread(target=receive_content)
+    thread_receive.start()
+    thread_send = threading.Thread(target=handle_input_content)
+    thread_send.start()
+
+main()
