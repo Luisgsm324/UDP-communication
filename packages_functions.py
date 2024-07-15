62ababe865ba4cb3239653c016d98674050e6c46
Luisgsm324 <luisf.moreira324@gmail.com>
2024-07-15 20:15:01 -0300

    [feat]: Criação da estrutura básica do cálculo checksum para o remetente

--- packages_functions.py
+++ packages_functions.py
@@ -1,3 +1,4 @@
+import math
 
 def send_for_clients(type_user, clients, adress, sendto, data):
     if type_user == "server":
@@ -6,6 +7,22 @@ def send_for_clients(type_user, clients, adress, sendto, data):
                 sendto(data, client)
     else:
         sendto(data, adress)
+
+def checksum_sender_calculator(content, block_length):
+    # Processo de divisão em blocos do pacote
+    number_blocks = math.ceil(len(content)/block_length)
+    start = 0
+    end = block_length
+    values = []
+    for _ in range(number_blocks):
+        checksum_value = content[start:end]
+        print(checksum_value)
+        start = end
+        end += block_length
+        values.append(checksum_value)
+
+    return values
+
 
 def send_packages(content, sendto, type_user, filetxt, adress, clients = []):
     if type_user == "client":
@@ -16,7 +33,9 @@ def send_packages(content, sendto, type_user, filetxt, adress, clients = []):
         while True:
             # Monta pacote
             data = file1.read(500)
-            
+            number = checksum_sender_calculator(data, 16)
+            print(len(data), number)
+            #print(len(data))
             if not data: break
             # Envia
             send_for_clients(type_user, clients, adress, sendto, data)
